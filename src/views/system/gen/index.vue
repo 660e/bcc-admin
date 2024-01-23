6f3b4bc067d9d15bdd0ec2a936e5882530064a5b
Min <solunar@vip.qq.com>
2024-01-23 14:54:18 +0800

    feat: 初始化“代码生成-编辑”静态页面

--- src/views/system/gen/index.vue
+++ src/views/system/gen/index.vue
@@ -1,5 +1,6 @@
 <script lang="ts" name="gen-manage" setup>
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 import { ColumnProps } from '@/components/pro-table/interface';
 
 import ProTable from '@/components/pro-table/index.vue';
@@ -29,6 +30,11 @@ const importDataConfirm = (ids: string[]) => {
 const batchDelete = (ids: string[]) => {
   console.log(ids);
 };
+
+const $router = useRouter();
+const edit = (id: string) => {
+  $router.push({ name: 'gen-edit', params: { id } });
+};
 </script>
 
 <template>
@@ -38,9 +44,9 @@ const batchDelete = (ids: string[]) => {
         <el-button @click="importData">导入</el-button>
         <el-button @click="batchDelete(scope.selectedListIds)">删除</el-button>
       </template>
-      <template #operation>
+      <template #operation="scope">
         <el-button type="primary" link>预览</el-button>
-        <el-button type="primary" link>编辑</el-button>
+        <el-button @click="edit(scope.row.id)" type="primary" link>编辑</el-button>
         <el-button type="primary" link>删除</el-button>
         <el-button type="primary" link>同步</el-button>
         <el-button type="primary" link>生成代码</el-button>
---
+++ src/views/system/gen/pages/edit.vue
@@ -0,0 +1,5 @@
+<script lang="ts" name="edit-page" setup></script>
+
+<template>
+  <div>page</div>
+</template>
