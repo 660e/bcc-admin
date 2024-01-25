ceb6ca1b759df07113b2d0e043782563a1add2e6
Min <solunar@vip.qq.com>
2024-01-25 15:39:51 +0800

    feat: “代码生成-编辑-基本信息”补充字段

--- src/views/system/gen/pages/panes/info.vue
+++ src/views/system/gen/pages/panes/info.vue
@@ -3,31 +3,23 @@ import { reactive } from 'vue';
 import type { FormRules } from 'element-plus';
 
 interface Forms {
-  tableName: string;
-  tableComment: string;
-  className: string;
-  tplCategory: string;
-  packageName: string;
-  moduleName: string;
-  businessName: string;
-  functionName: string;
-  remark: string;
-
-  functionAuthor: string;
-  tplWebType: string;
-  genType: string;
+  [key: string]: string;
 }
 
 const forms = reactive<Forms>({
   tableName: '',
   tableComment: '',
-  className: '',
   tplCategory: 'crud',
+  className: '',
   packageName: '',
   moduleName: '',
   businessName: '',
   functionName: '',
   remark: '',
+
+  treeCode: '',
+  treeParentCode: '',
+  treeName: '',
 
   functionAuthor: 'admin',
   tplWebType: 'element-plus',
@@ -41,22 +33,26 @@ const rules = reactive<FormRules<Forms>>({
   packageName: [{ required: true, message: '请填写生成包路径', trigger: 'blur' }],
   moduleName: [{ required: true, message: '请填写生成模块名', trigger: 'blur' }],
   businessName: [{ required: true, message: '请填写生成业务名', trigger: 'blur' }],
-  functionName: [{ required: true, message: '请填写生成功能名', trigger: 'blur' }]
+  functionName: [{ required: true, message: '请填写生成功能名', trigger: 'blur' }],
+
+  treeCode: [{ required: true, message: '请填写树编码字段', trigger: 'change' }],
+  treeParentCode: [{ required: true, message: '请填写树父编码字段', trigger: 'change' }],
+  treeName: [{ required: true, message: '请填写树名称字段', trigger: 'change' }],
+
+  subTableName: [{ required: true, message: '请填写关联子表的表名', trigger: 'change' }],
+  subTableFkName: [{ required: true, message: '请填写子表关联的外键名', trigger: 'change' }]
 });
 </script>
 
 <template>
   <el-tab-pane v-bind="$attrs">
-    <el-form :model="forms" :rules="rules" label-width="130px">
-      <div class="grid grid-cols-2 grid-rows-4 grid-flow-col pt-5">
+    <el-form :model="forms" :rules="rules" label-width="170px">
+      <div class="grid grid-cols-2 pt-5">
         <el-form-item label="表名称" prop="tableName">
           <el-input v-model="forms.tableName" />
         </el-form-item>
         <el-form-item label="表描述" prop="tableComment">
           <el-input v-model="forms.tableComment" />
-        </el-form-item>
-        <el-form-item label="实体类名称" prop="className">
-          <el-input v-model="forms.className" />
         </el-form-item>
         <el-form-item label="生成模板">
           <el-select v-model="forms.tplCategory">
@@ -64,6 +60,9 @@ const rules = reactive<FormRules<Forms>>({
             <el-option label="树表（增删改查）" value="tree" />
             <el-option label="主子表（增删改查）" value="sub" />
           </el-select>
+        </el-form-item>
+        <el-form-item label="实体类名称" prop="className">
+          <el-input v-model="forms.className" />
         </el-form-item>
         <el-form-item prop="packageName">
           <template #label>
@@ -109,7 +108,71 @@ const rules = reactive<FormRules<Forms>>({
           </template>
           <el-input v-model="forms.functionName" />
         </el-form-item>
+
+        <!-- 生成模板：树表 -->
+        <template v-if="forms.tplCategory === 'tree'">
+          <el-form-item prop="treeCode">
+            <template #label>
+              <div class="flex items-center space-x-1">
+                <span>树编码字段</span>
+                <el-tooltip effect="dark" content="树显示的编码字段名，如：dept_id" placement="top">
+                  <el-icon><QuestionFilled /></el-icon>
+                </el-tooltip>
+              </div>
+            </template>
+            <el-select v-model="forms.treeCode"></el-select>
+          </el-form-item>
+          <el-form-item prop="treeParentCode">
+            <template #label>
+              <div class="flex items-center space-x-1">
+                <span>树父编码字段</span>
+                <el-tooltip effect="dark" content="树显示的父编码字段名，如：parent_Id" placement="top">
+                  <el-icon><QuestionFilled /></el-icon>
+                </el-tooltip>
+              </div>
+            </template>
+            <el-select v-model="forms.treeParentCode"></el-select>
+          </el-form-item>
+          <el-form-item prop="treeName">
+            <template #label>
+              <div class="flex items-center space-x-1">
+                <span>树名称字段</span>
+                <el-tooltip effect="dark" content="树节点的显示名称字段名，如：dept_name" placement="top">
+                  <el-icon><QuestionFilled /></el-icon>
+                </el-tooltip>
+              </div>
+            </template>
+            <el-select v-model="forms.treeName"></el-select>
+          </el-form-item>
+        </template>
+
+        <!-- 生成模板：主子表 -->
+        <template v-if="forms.tplCategory === 'sub'">
+          <el-form-item prop="subTableName">
+            <template #label>
+              <div class="flex items-center space-x-1">
+                <span>关联子表的表名</span>
+                <el-tooltip effect="dark" content="关联子表的表名，如：sys_user" placement="top">
+                  <el-icon><QuestionFilled /></el-icon>
+                </el-tooltip>
+              </div>
+            </template>
+            <el-select v-model="forms.subTableName"></el-select>
+          </el-form-item>
+          <el-form-item prop="subTableFkName">
+            <template #label>
+              <div class="flex items-center space-x-1">
+                <span>子表关联的外键名</span>
+                <el-tooltip effect="dark" content="子表关联的外键名，如：user_id" placement="top">
+                  <el-icon><QuestionFilled /></el-icon>
+                </el-tooltip>
+              </div>
+            </template>
+            <el-select v-model="forms.subTableFkName"></el-select>
+          </el-form-item>
+        </template>
       </div>
+
       <el-form-item label="备注">
         <el-input v-model="forms.remark" :rows="5" type="textarea" />
       </el-form-item>
