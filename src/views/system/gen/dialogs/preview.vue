373a244444f85264d9c290c201bf21dbe339e446
Min <solunar@vip.qq.com>
2024-01-26 15:33:06 +0800

    feat: “代码生成-预览”静态页面

--- src/views/system/gen/dialogs/preview.vue
+++ src/views/system/gen/dialogs/preview.vue
@@ -10,6 +10,9 @@ const closed = () => {
 };
 
 const active = ref('index.vue');
+const copy = () => {
+  console.log(active.value);
+};
 
 defineExpose({ open });
 </script>
@@ -18,12 +21,16 @@ defineExpose({ open });
   <el-dialog v-model="visible" @closed="closed" title="代码预览" fullscreen>
     <el-tabs v-model="active" tab-position="left" class="tabs">
       <el-tab-pane label="index.vue" name="index.vue">
-        <div style="height: 3000px">Lorem ipsum dolor sit amet.</div>
+        <div class="p-5" style="height: 3000px">Lorem ipsum dolor sit amet.</div>
       </el-tab-pane>
       <el-tab-pane label="index.ts" name="index.ts">
-        <div style="height: 2000px">Lorem ipsum dolor sit amet.</div>
+        <div class="p-5" style="height: 2000px">Lorem ipsum dolor sit amet.</div>
       </el-tab-pane>
     </el-tabs>
+    <div @click="copy" class="fixed top-[61px] right-5 flex items-center space-x-1 cursor-pointer">
+      <el-icon><CopyDocument /></el-icon>
+      <span>复制</span>
+    </div>
   </el-dialog>
 </template>
 
