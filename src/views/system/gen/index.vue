27033f0edcdd349cc97af1466204913a469dfa2c
Min <solunar@vip.qq.com>
2024-01-26 10:47:03 +0800

    feat: “代码生成”初始化删除

--- src/views/system/gen/index.vue
+++ src/views/system/gen/index.vue
@@ -2,17 +2,18 @@
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { ColumnProps } from '@/components/pro-table/interface';
+import { ElMessage, ElMessageBox } from 'element-plus';
 
 import ProTable from '@/components/pro-table/index.vue';
 import ImportDialog from './dialogs/import.vue';
 
-const data = ref(new Array(20).fill(null).map((e, i) => ({ id: i, name: `TEST-G-${i}` })));
+const data = ref(new Array(20).fill(null).map((e, i) => ({ id: i, tableName: `TEST-G-${i}` })));
 const columns: ColumnProps[] = [
   { type: 'selection', fixed: 'left', width: 0 },
-  { prop: 'name', label: '表名称', search: { el: 'input' } },
-  { prop: 'description', label: '表描述', search: { el: 'input' } },
-  { prop: 'substance', label: '实体' },
-  { prop: 'creationTime', label: '创建时间' },
+  { prop: 'tableName', label: '表名称', search: { el: 'input' } },
+  { prop: 'tableComment', label: '表描述', search: { el: 'input' } },
+  { prop: 'className', label: '实体' },
+  { prop: 'createTime', label: '创建时间' },
   { prop: 'updateTime', label: '更新时间' },
   { prop: 'operation', label: '操作', fixed: 'right', width: 0 }
 ];
@@ -32,6 +33,14 @@ const $router = useRouter();
 const edit = (id: string) => {
   $router.push({ name: 'gen-edit', params: { id } });
 };
+const remove = (row: any) => {
+  ElMessageBox.confirm(`是否确认删除表名称为“${row.tableName}”的数据项？`, '系统提示', { type: 'warning' }).then(() => {
+    ElMessage({
+      type: 'success',
+      message: '删除成功'
+    });
+  });
+};
 </script>
 
 <template>
@@ -44,7 +53,7 @@ const edit = (id: string) => {
       <template #operation="scope">
         <el-button type="primary" link>预览</el-button>
         <el-button @click="edit(scope.row.id)" type="primary" link>编辑</el-button>
-        <el-button type="primary" link>删除</el-button>
+        <el-button @click="remove(scope.row)" type="primary" link>删除</el-button>
         <el-button type="primary" link>同步</el-button>
         <el-button type="primary" link>生成代码</el-button>
       </template>
