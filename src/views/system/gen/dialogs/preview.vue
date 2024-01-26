9656e90419f152a1869e7ef64b4d104a806eb2f4
Min <solunar@vip.qq.com>
2024-01-26 15:22:16 +0800

    feat: “代码生成-预览”静态页面

--- src/views/system/gen/dialogs/preview.vue
+++ src/views/system/gen/dialogs/preview.vue
@@ -9,11 +9,31 @@ const closed = () => {
   console.log('closed');
 };
 
+const active = ref('index.vue');
+
 defineExpose({ open });
 </script>
 
 <template>
   <el-dialog v-model="visible" @closed="closed" title="代码预览" fullscreen>
-    <div>Lorem ipsum dolor sit amet.</div>
+    <el-tabs v-model="active" tab-position="left" class="tabs">
+      <el-tab-pane label="index.vue" name="index.vue">
+        <div style="height: 3000px">Lorem ipsum dolor sit amet.</div>
+      </el-tab-pane>
+      <el-tab-pane label="index.ts" name="index.ts">
+        <div style="height: 2000px">Lorem ipsum dolor sit amet.</div>
+      </el-tab-pane>
+    </el-tabs>
   </el-dialog>
 </template>
+
+<style lang="scss" scoped>
+$height: calc(100vh - 41px);
+.tabs {
+  height: $height;
+  :deep(.el-tabs__content) {
+    height: $height;
+    overflow-y: auto;
+  }
+}
+</style>
