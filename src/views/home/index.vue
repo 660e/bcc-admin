04902918fe088e0e299525c90fd879d172a95b86
Min <solunar@vip.qq.com>
2024-02-02 16:55:40 +0800

    refactor: 移除未使用的静态资源

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="card h-full flex justify-center items-center">
-    <img src="@/assets/images/welcome.png" class="w-3/4" />
-  </div>
+  <div class="card h-full"></div>
 </template>
 
 <script lang="ts" name="home" setup></script>
