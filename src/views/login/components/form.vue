e6f3172801d41f125440ca41c77abea6d336fb6f
Min <solunar@vip.qq.com>
2024-02-04 17:30:13 +0800

    feat: 登录页接口联调

--- src/views/login/components/form.vue
+++ src/views/login/components/form.vue
@@ -1,5 +1,6 @@
 <script lang="ts" name="login-form" setup>
-import { reactive } from 'vue';
+import { onMounted, reactive, ref } from 'vue';
+import { FullScreen, Lock, User } from '@element-plus/icons-vue';
 import { getCaptcha } from '@/api/modules/login';
 
 const forms = reactive({
@@ -8,14 +9,30 @@ const forms = reactive({
   captcha: ''
 });
 
-getCaptcha().then(response => {
+const captchaImage = ref('');
+const refreshCaptcha = () => {
+  getCaptcha().then((response: any) => {
+    captchaImage.value = `data:image/gif;base64,${response.img}`;
+  });
+};
+const login = () => {
   console.log(forms);
-  console.log(response);
+};
+
+onMounted(() => {
+  refreshCaptcha();
 });
 </script>
 
 <template>
-  <div>
-    <div>BCC-Admin</div>
+  <div class="p-5 space-y-5">
+    <div class="text-xl text-center leading-none">BCC-Admin</div>
+    <el-input v-model="forms.username" :prefix-icon="User" placeholder="账号" size="large" />
+    <el-input v-model="forms.password" :prefix-icon="Lock" placeholder="密码" size="large" />
+    <div class="flex space-x-5">
+      <el-input v-model="forms.captcha" :prefix-icon="FullScreen" placeholder="验证码" size="large" />
+      <el-image :src="captchaImage" @click="refreshCaptcha" class="cursor-pointer" />
+    </div>
+    <el-button @click="login" type="primary" class="w-full" size="large">登录</el-button>
   </div>
 </template>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="h-full flex justify-center items-center relative">
-    <div class="flex rounded overflow-hidden">
-      <el-image :src="loginImage" fit="cover" class="w-80" />
-      <login-form class="w-80" />
+    <div class="flex rounded overflow-hidden border border-gray-300">
+      <el-image :src="loginImage" fit="cover" class="w-96" />
+      <login-form class="w-96" />
     </div>
     <switch-dark class="absolute right-5 bottom-5" />
   </div>
