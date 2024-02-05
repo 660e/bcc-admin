9b757bbeec674097b5afd366e7df1c77b7aa00da
Min <solunar@vip.qq.com>
2024-02-05 17:16:42 +0800

    feat: “菜单管理-删除”接口联调

--- src/api/modules/system.ts
+++ src/api/modules/system.ts
@@ -1,5 +1,11 @@
 import http from '@/api';
 
+// 获取菜单列表
 export function getMenuList(params: any) {
   return http.get('/system/menu/list', params);
+}
+
+// 删除菜单
+export function deleteMenu(id: string) {
+  return http.delete(`/system/menu/${id}`);
 }
--- src/views/system/menu/index.vue
+++ src/views/system/menu/index.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="flex flex-col h-full">
-    <pro-table :columns="columns" :request-api="requestApi" :pagination="false" row-key="menuId">
+    <pro-table :columns="columns" :request-api="requestApi" :pagination="false" row-key="menuId" ref="tableRef">
       <template #tableHeader>
         <el-button type="primary">新增</el-button>
       </template>
-      <template #operation>
+      <template #operation="scope">
         <el-button type="primary" link>编辑</el-button>
         <el-button type="primary" link>新增</el-button>
-        <el-button type="primary" link>删除</el-button>
+        <el-button @click="remove(scope.row)" type="primary" link>删除</el-button>
       </template>
     </pro-table>
   </div>
 </template>
 
 <script lang="ts" name="menu-manage" setup>
-import { getMenuList } from '@/api/modules/system';
+import { ref } from 'vue';
+import { ElMessage, ElMessageBox } from 'element-plus';
+import { getMenuList, deleteMenu } from '@/api/modules/system';
 import { buildTree } from '@/utils';
 import { ColumnProps } from '@/components/pro-table/interface';
 
 import ProTable from '@/components/pro-table/index.vue';
 
+const tableRef = ref();
 const columns: ColumnProps[] = [
   { prop: 'menuName', label: '菜单名称', width: 200, search: { el: 'input' } },
   { prop: 'icon', label: '图标', width: 120 },
@@ -37,5 +40,15 @@ const requestApi = (params: any) => {
     const data = buildTree(response.data, 'menuId', 'parentId');
     resolve({ data });
   });
+};
+
+const remove = (row: any) => {
+  ElMessageBox.confirm(`是否删除“${row.menuName}”？`, '系统提示', { type: 'warning' })
+    .then(async () => {
+      await deleteMenu(row.menuId);
+      tableRef.value.search();
+      ElMessage({ type: 'success', message: '删除成功' });
+    })
+    .catch(() => false);
 };
 </script>
