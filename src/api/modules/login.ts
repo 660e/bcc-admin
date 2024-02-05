7f4c4b548f559b5643adb4065b1d5d44fd5655f1
Min <solunar@vip.qq.com>
2024-02-05 11:35:51 +0800

    feat: 整理登录页相关代码

--- src/api/modules/login.ts
+++ src/api/modules/login.ts
@@ -1,7 +1,6 @@
 import http from '@/api';
 import { Login } from '@/api/interface/index';
 
-import { PORT1 } from '@/api/config/servicePort';
 import authMenuList from '@/assets/json/authMenuList.json';
 import authButtonList from '@/assets/json/authButtonList.json';
 
@@ -13,23 +12,14 @@ export function getCode() {
 // 登录
 export function login(params: Login.Form) {
   return http.post<Login.Response>('/auth/login', params);
+  // return http.post<Login.Response>('/auth/login', {}, { params }); // post携带query -> ?username=admin&password=admin123
+  // return http.post<Login.Response>('/auth/login', qs.stringify(params)); // post携带表单参数 -> application/x-www-form-urlencoded
+  // return http.get<Login.Response>(`/auth/login?${qs.stringify(params, { arrayFormat: 'repeat' })}`); // get携带复杂参数
 }
 
 //
 //
 //
-
-/**
- * @name 登录模块
- */
-// 用户登录
-export const loginApi = (params: Login.ReqLoginForm) => {
-  return http.post<Login.ResLogin>(PORT1 + `/login`, params, { loading: false }); // 正常 post json 请求  ==>  application/json
-  // return http.post<Login.ResLogin>(PORT1 + `/login`, params, { loading: false }); // 控制当前请求不显示 loading
-  // return http.post<Login.ResLogin>(PORT1 + `/login`, {}, { params }); // post 请求携带 query 参数  ==>  ?username=admin&password=123456
-  // return http.post<Login.ResLogin>(PORT1 + `/login`, qs.stringify(params)); // post 请求携带表单参数  ==>  application/x-www-form-urlencoded
-  // return http.get<Login.ResLogin>(PORT1 + `/login?${qs.stringify(params, { arrayFormat: "repeat" })}`); // get 请求可以携带数组等复杂参数
-};
 
 // 获取菜单列表
 export const getAuthMenuListApi = () => {
@@ -47,5 +37,5 @@ export const getAuthButtonListApi = () => {
 
 // 用户退出登录
 export const logoutApi = () => {
-  return http.post(PORT1 + `/logout`);
+  return http.post(`/logout`);
 };
